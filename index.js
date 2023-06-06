d7df08a72d3d437a80c8e7bffcd370f4ab485987
Jason He <jasonhe.md@gmail.com>
2023-06-05 22:21:57 -0700

    Bubble redesign

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
-    require('dotenv').config();
-    const OpenAI = require('openai');
-    const { Configuration, OpenAIApi } = OpenAI;
+require('dotenv').config();
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI;
 
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const cors = require('cors');
-    const app = express();
-    const port = 3001;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const session = require('express-session');
+const app = express();
+const port = 3001;
 
-    const configuration = new Configuration({
-        organization: "org-nG55UrIJBArh3cNhcpw0Tfrv",
-        apiKey: process.env.OPENAI_SECRET_KEY,
+const configuration = new Configuration({
+    organization: "org-nG55UrIJBArh3cNhcpw0Tfrv",
+    apiKey: process.env.OPENAI_SECRET_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.post('/', async (req, res) => {
+    const { message } = req.body;
+
+    // If there's no conversation history for the session, initialize it
+    if (!req.session.messages) {
+        req.session.messages = [
+            {role: "system", content: process.env.SYSTEM_PROMPT},
+            {role: "user", content: message},
+        ];
+    } else {
+        // Append the new user message to the history
+        req.session.messages.push({role: "user", content: message});
+    }
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: req.session.messages,
+        max_tokens: 100,
     });
 
-    const openai = new OpenAIApi(configuration)
+    if (response.data.choices[0].message.content) {
+        // Append the new bot message to the history
+        req.session.messages.push({role: "assistant", content: response.data.choices[0].message.content});
 
-    app.use(cors());
-    app.use(bodyParser.json());
-
-    app.post('/', async (req, res) => {
-        const { message } = req.body;
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {role: "system", content: process.env.SYSTEM_PROMPT},
-                {role: "user", content: message},
-            ],
-            max_tokens: 100,
+        res.json({
+            message: response.data.choices[0].message.content
         });
-        if (response.data.choices[0].message.content) {
-            res.json({
-                message: response.data.choices[0].message.content
-            });
-        }
-    });
+    }
+});
 
-    app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-    });
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function MessageBubble({ text }) {
+  return (
+    <div className="bubble-sender">
+      {text}
+    </div>
+  );
+}
+
+function ResponseBubble({ text }) {
+  return (
+    <div className="bubble-receiver">
+      {text}
+    </div>
+  );
+}
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [response, setResponse] = useState("");
+  const [newMessage, setNewMessage] = useState("");
+  const [convo, setConvo] = useState([]);
 
   const handleSubmit = async e => {
     e.preventDefault();
-    fetch('http://localhost:3001/', {
+    const response = await fetch('http://localhost:3001/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ message }), 
-    })
-      .then((res) => res.json())
-      .then((data) => setResponse(data.message))
+      body: JSON.stringify({ message: newMessage }), 
+    }).then(res => res.json())
+
+    setConvo([...convo, { message: newMessage, response: response.message }]);
+    setNewMessage('');
   };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <h1>Text-Savvy</h1>
+    <div className="App flex flex-col justify-between h-screen bg-gray-100 p-5">
+      <h1 className="text-3xl mb-4">Text-Savvy</h1>
+      <div className="flex flex-col overflow-auto mb-4">
+        {convo.map((item, index) => (
+          <React.Fragment key={index}>
+            <MessageBubble text={item.message} />
+            <ResponseBubble text={item.response} />
+          </React.Fragment>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="flex">
         <textarea
-          rows="10"
-          cols="35"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
+          className="flex-grow mr-2 py-2 px-3 rounded bg-white shadow-inner resize-none"
+          rows="1"
+          placeholder='Type a message...'
+          value={newMessage}
+          onChange={e => setNewMessage(e.target.value)}
         />
-        <button type='submit'>Submit</button>
-        <p>{response}</p>
+        <button type='submit' className="py-2 px-6 rounded bg-blue-500 text-white">
+          Submit
+        </button>
       </form>
     </div>
   );
