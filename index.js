f5c92884a0ce02636a76cab683a04f30cb9d5abf
Jason He <jasonhe.md@gmail.com>
2023-06-10 20:56:30 -0700

    Send entire chat log to API

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 const promptScorer = 'You are a rater of my text messages to a girl. Rate it on a scale of 1 to 10, and give me a reasoning as to why I got that message. Assume my goal is to become closer with this girl and potentially get in a relationship. Return your examples in a json object with keys "score" and "reason". Do not add any additional commentary besides the json. \n\nExample:\n\nInput:\nPrevious Message: how are you\nMy Message: good, how was your day tho?\n\nOutput:\n{\n"score": "7",\n"reason": "You were too direct and potentially acting too fast. That previous message maybe wanted you to talk about yourself"\n}';
 const promptResponder = 'You are a young adult female who is texting a guy that is interested in you. You are not interested in him and want to end the conversation. However, you try to be polite until he starts to become aggressive. Text in lower case, use slang, emojis, abbreviations, short sentences, and gramatical errors to make the conversation more realistic (only use emojis when appropriate, not on every message). The conversation ends when he stops texting you. Act like a really dry texter and make it hard for him to figure out a response because your responses are so short. When you do not know the answer, simply say idk. Only respond with the next text in the sequence, assume the most recent text was from him.';
 let previousMessage = '';
-let sum = 0;
+let messageArray = [];
 
 // Define Express application settings
 app.use(cors());
@@ -36,15 +36,15 @@ app.use(session({
     saveUninitialized: true
 }));
 
-const initializeMessages = (req, message) => {
-    req.session.messages = [
+const initializeMessages = (message) => {
+    messageArray = [
         { role: 'system', content: promptResponder },
         { role: 'user', content: message },
     ];
 };
 
-const appendUserMessage = (req, message) => {
-    req.session.messages.push({ role: 'user', content: message });
+const appendUserMessage = (message) => {
+    messageArray.push({ role: 'user', content: message });
 };
 
 
@@ -66,32 +66,30 @@ const rateMessage = async (message) => {
 }
 
 app.post('/', async (req, res) => {
-    sum++;
-    console.log(sum);
     const { message } = req.body;
-    // Request scoring from OpenAI
-    const responseScore = await rateMessage(message);
-    console.log(responseScore.data.choices[0].message.content);
-
-    if (!req.session.messages) {
-        initializeMessages(req, message);
+    console.log(message);
+    if (messageArray.length === 0) {
+        initializeMessages(message);
     } else {
         // Append user message to the history
-        appendUserMessage(req, message);
+        appendUserMessage(message);
     }
-    console.log(req.session.messages);
-    // Parse and process score response
-    const scoreJSON = responseScore.data.choices[0].message.content;
+
+    console.log(messageArray);
+
     try {
+        const responseScore = await rateMessage(message);
+        const scoreJSON = responseScore.data.choices[0].message.content;
         const jsonObject = JSON.parse(scoreJSON);
+
         if (jsonObject.score > 4) {
-            const response = await createChatCompletion(req.session.messages);
+            const response = await createChatCompletion(messageArray);
 
             // Process and append assistant message to the history
             const responseMessage = response.data.choices[0].message.content;
             previousMessage = responseMessage
             if (responseMessage) {
-                req.session.messages.push({ role: 'assistant', content: responseMessage });
+                messageArray.push({ role: 'assistant', content: responseMessage });
                 res.json({ message: responseMessage, jsonObject: jsonObject });
             }
         }
@@ -100,7 +98,6 @@ app.post('/', async (req, res) => {
         }
     } catch (e) {
         console.error('Error:', e);
-        console.log(scoreJSON);
     }
 });
 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,11 +5,13 @@ export default function Banner({ jsonObject, closeBanner }) {
 
   return (
     <div className={`z-50 fixed top-0 left-0 w-full text-white p-4 flex justify-between items-center space-x-4 ${jsonObject.score <= 4 ? 'bg-red-500' : jsonObject.score <= 7 ? 'bg-yellow-500' : 'bg-green-500'}`}>
-      <div>
+      <div className="flex flex-col justify-center items-center">
         <h2 className="font-bold">Score: {jsonObject.score}</h2>
         <p>Reason: {jsonObject.reason}</p>
       </div>
-      <button onClick={closeBanner} className="bg-white text-black p-2 rounded">Close</button>
+      <div className="flex justify-end">
+        <button onClick={closeBanner} className="bg-white text-black p-2 rounded">Close</button>
+      </div>
     </div>
   );
 }
