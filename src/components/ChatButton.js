b72458fd990e2d28bcf30a3b8e732a762eaa0024
Jason He <jasonhe.md@gmail.com>
2023-06-28 22:53:27 -0700

    Fix overflow and add rounded selection/hover

--- src/components/ChatButton.js
+++ src/components/ChatButton.js
@@ -4,12 +4,12 @@ function ChatButton({ mode, selectedChat, changeMode }) {
   const isSelected = mode === selectedChat;
   return (
     <div
-      className={`flex flex-row items-center p-3 cursor-pointer ${isSelected ? 'bg-blue-500 text-white' : 'bg-white text-gray-900'} hover:bg-gray-200`}
+      className={`flex flex-row items-center p-3 cursor-pointer ${isSelected ? 'bg-blue-500 text-white rounded-lg' : 'bg-white text-gray-900'} hover:bg-gray-200 hover:rounded-lg`}
       onClick={() => changeMode(mode)}
     >
       <div className="flex-shrink-0 w-12 h-12 rounded-full bg-gray-300 mr-3"></div>
-      <div className="flex-grow">
-        <div className="flex flex-row justify-between">
+      <div className="flex-grow truncate">
+        <div className="flex flex-row justify-between ">
           <h2 className="font-bold text-lg">{mode}</h2>
           <p className="text-sm">12:34 PM</p>
         </div>
--- src/components/ChatButtonList.js
+++ src/components/ChatButtonList.js
@@ -3,7 +3,7 @@ import ChatButton from './ChatButton';
 
 function ChatButtonList({ chatModes, selectedChat, changeMode }) {
   return (
-    <div className="flex flex-col overflow-y-auto">
+    <div className="flex flex-col overflow-y-auto bg-white">
       {chatModes.map(mode => (
         <ChatButton key={mode} mode={mode} selectedChat={selectedChat} changeMode={changeMode} />
       ))}
