e7b18789a7fb09d2727bc9d2978f66b798c7bd88
Jason He <jasonhe.md@gmail.com>
2023-06-28 23:32:24 -0700

    Add pictures

--- src/components/ChatButton.js
+++ src/components/ChatButton.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-function ChatButton({ mode, selectedChat, changeMode }) {
+function ChatButton({ mode, selectedChat, changeMode, picture = 'blankprofile.png' }) {
   const isSelected = mode === selectedChat;
   return (
     <div
-      className={`flex flex-row items-center p-3 cursor-pointer ${isSelected ? 'bg-blue-500 text-white' : 'bg-white text-gray-900 hover:bg-gray-200'} rounded-lg`}
+      className={`flex flex-row items-center p-3 cursor-pointer ${isSelected ? 'bg-blue-500 text-white'
+              : 'bg-white text-gray-900 hover:bg-gray-200'} rounded-lg`}
       onClick={() => changeMode(mode)}
     >
-      <div className="flex-shrink-0 w-12 h-12 rounded-full bg-gray-300 mr-3"></div>
+      <div className="flex-shrink-0 w-12 h-12 rounded-full bg-gray-300 mr-3">
+        <img src={picture} alt="Profile" className="w-full h-full rounded-full object-cover" />
+      </div>
       <div className="flex-grow truncate">
         <div className="flex flex-row justify-between ">
           <h2 className="font-bold text-lg">{mode}</h2>
